2f249516bb579700d2dbcec790b01c740dded066
TheTechromancer <TheTechromancer@null.net>
2019-12-09 13:25:03 -0500

    install nvidia docker runtime

--- crackhaws.sh
+++ crackhaws.sh
@@ -4,13 +4,18 @@
 
 logfile='/var/log/nvidia_driver_install.log'
 
+# skip prompts in apt-upgrade, etc.
+export DEBIAN_FRONTEND=noninteractive
+alias apt-get='-o Dpkg::Options::="--force-confdef" -y'
+
+
 check_root()
 {
 
-	if [ $EUID -ne 0 ]; then
-		printf '\n[!] Please sudo me!\n\n'
-		exit 1
-	fi
+    if [ $EUID -ne 0 ]; then
+        printf '\n[!] Please sudo me!\n\n'
+        exit 1
+    fi
 
 }
 
@@ -18,18 +23,16 @@ check_root()
 get_latest_driver_version()
 {
 
-	printf "\n[+] Getting latest driver version from Nvidia's website\n"
+    printf "\n[+] Getting latest driver version from Nvidia's website\n"
 
-	# gnarly curl command
-	# queries Nvidia's download page and carves version number from HTML table
-	# Tesla, Linux x86_64
-	# egrep -o '<td class="gridItem">[0-9]{3}\.[0-9]{2}</td>' | head -n 1 |
-	# cut -d'>' -f2 | cut -d'<' -f1)
-	latest_driver_version=$(curl -b 'ProductSeriesType_WHQL=7; ProductSeries_WHQL=105; ProductType_WHQL=857; OperatingSystem_WHQL=12; Language_WHQL=1; WHQL_WHQL=' 'https://www.nvidia.com/Download/driverResults.aspx/136954/en-us' 2>/dev/null |
-	egrep -ow '3[0-9]{2}\.[0-9]{2}' | sort -u -n -r | head -n 1)
+    # gnarly curl command
+    # queries Nvidia's download page and carves version number from HTML table
+    # Tesla, Linux x86_64
+    latest_driver_version=$(curl -b 'ProductSeriesType_WHQL=7; ProductSeries_WHQL=105; ProductType_WHQL=857; OperatingSystem_WHQL=12; Language_WHQL=1; WHQL_WHQL=' 'https://www.nvidia.com/Download/driverResults.aspx/136954/en-us' 2>/dev/null |
+    egrep -ow '3[0-9]{2}\.[0-9]{2}' | sort -u -n -r | head -n 1)
 
 
-	printf "\n[+] Latest driver version is $latest_driver_version\n"
+    printf "\n[+] Latest driver version is $latest_driver_version\n"
 
 }
 
@@ -37,19 +40,19 @@ get_latest_driver_version()
 package_install()
 {
 
-	printf '\n[+] Updating system\n'
-	printf '    - apt-get update\n'
-	apt-get -y update 2>&1 | tee -a "$logfile"
-	printf '    - apt-get upgrade\n'
-	apt-get -y upgrade 2>&1 | tee -a "$logfile"
+    printf '\n[+] Updating system\n'
+    printf '    - apt-get update\n'
+    apt-get update 2>&1 | tee -a "$logfile"
+    printf '    - apt-get upgrade\n'
+    apt-get upgrade 2>&1 | tee -a "$logfile"
 
-	printf '\n[+] Installing dependencies\n'
-	printf "    - apt-get install build-essential linux-headers-$(uname -r) p7zip-full\n"
-	apt-get -y install build-essential linux-headers-$(uname -r) p7zip-full >>"$logfile" 2>&1
+    printf '\n[+] Installing dependencies\n'
+    printf "    - apt-get install build-essential linux-headers-$(uname -r) p7zip-full\n"
+    apt-get install build-essential linux-headers-$(uname -r) p7zip-full >>"$logfile" 2>&1
 
-	# dependencies for hcxtools and hcxdumptool
-	# printf "    - apt-get -y install libssl-dev zlib1g-dev libcurl4-openssl-dev\n"
-	# apt-get -y install libssl-dev zlib1g-dev libcurl4-openssl-dev >>"$logfile" 2>&1
+    # dependencies for hcxtools and hcxdumptool
+    # printf "    - apt-get install libssl-dev zlib1g-dev libcurl4-openssl-dev\n"
+    # apt-get install libssl-dev zlib1g-dev libcurl4-openssl-dev >>"$logfile" 2>&1
 
 }
 
@@ -57,25 +60,25 @@ package_install()
 hashcat_install()
 {
 
-	# get download URL
-	hashcat_download_link=$(curl 'https://hashcat.net/hashcat/' 2>>"$logfile" | egrep -o '<a href="/files/hashcat-.*.7z">Download</a>' | cut -d'"' -f2 | cut -d'"' -f1 | head -n 1)
+    # get download URL
+    hashcat_download_link=$(curl 'https://hashcat.net/hashcat/' 2>>"$logfile" | egrep -o '<a href="/files/hashcat-.*.7z">Download</a>' | cut -d'"' -f2 | cut -d'"' -f1 | head -n 1)
 
-	printf "\n[+] Downloading latest hashcat version from https://hashcat.net$hashcat_download_link\n\n"
+    printf "\n[+] Downloading latest hashcat version from https://hashcat.net$hashcat_download_link\n\n"
 
-	# download archive
-	cd /opt
-	wget "https://hashcat.net$hashcat_download_link" || printf '\n[!] Failed to download hashcat\n\n'
+    # download archive
+    cd /opt
+    wget "https://hashcat.net$hashcat_download_link" || printf '\n[!] Failed to download hashcat\n\n'
 
-	# extract and delete archive
-	7z x 'hashcat-*.7z' >>"$logfile" 2>&1
-	rm hashcat-*.7z
-	#rm -r hashcat >/dev/null 2>&1
-	mv hashcat* hashcat
+    # extract and delete archive
+    7z x 'hashcat-*.7z' >>"$logfile" 2>&1
+    rm hashcat-*.7z
+    #rm -r hashcat >/dev/null 2>&1
+    mv hashcat* hashcat
 
-	printf '\n[+] Successfully installed hashcat to /opt/hashcat\n'
+    printf '\n[+] Successfully installed hashcat to /opt/hashcat\n'
 
-	# create symlink so it's in $PATH
-	ln -s /opt/hashcat/hashcat64.bin /usr/bin/hashcat 2>>"$logfile" && printf '\n[+] Successfully added hashcat to $PATH\n'
+    # create symlink so it's in $PATH
+    ln -s /opt/hashcat/hashcat64.bin /usr/bin/hashcat 2>>"$logfile" && printf '\n[+] Successfully added hashcat to $PATH\n'
 
 }
 
@@ -83,11 +86,11 @@ hashcat_install()
 driver_download()
 {
 
-	driver_url="http://us.download.nvidia.com/tesla/$latest_driver_version/NVIDIA-Linux-x86_64-$latest_driver_version.run"
+    driver_url="http://us.download.nvidia.com/tesla/$latest_driver_version/NVIDIA-Linux-x86_64-$latest_driver_version.run"
 
-	printf "\n[+] Downloading driver from $driver_url\n"
-	cd /root
-	wget $driver_url || (printf '\n[!] Failed to download driver\n'; exit 1)
+    printf "\n[+] Downloading driver from $driver_url\n"
+    cd /root
+    wget $driver_url || (printf '\n[!] Failed to download driver\n'; exit 1)
 
 }
 
@@ -95,11 +98,11 @@ driver_download()
 prepare_driver_install()
 {
 
-	service_file='/etc/systemd/system/nvidia_driver_install.service'
-	startup_script='/etc/systemd/system/nvidia_driver_install.sh'
+    service_file='/etc/systemd/system/nvidia_driver_install.service'
+    startup_script='/etc/systemd/system/nvidia_driver_install.sh'
 
-	# blacklist noveau (open-source driver)
-	cat <<EOF > /etc/modprobe.d/blacklist-nouveau.conf
+    # blacklist noveau (open-source driver)
+    cat <<EOF > /etc/modprobe.d/blacklist-nouveau.conf
 blacklist nouveau
 blacklist lbm-nouveau
 options nouveau modeset=0
@@ -107,15 +110,15 @@ alias nouveau off
 alias lbm-nouveau off
 EOF
 
-	modprobe_entry='options nouveau modeset=0'
-	if ! grep -xq "$modprobe_entry" /etc/modprobe.d/nouveau-kms.conf;
-	then
-		echo "$modprobe_entry" >> /etc/modprobe.d/nouveau-kms.conf
-	fi
-	update-initramfs -u
+    modprobe_entry='options nouveau modeset=0'
+    if ! grep -xq "$modprobe_entry" /etc/modprobe.d/nouveau-kms.conf;
+    then
+        echo "$modprobe_entry" >> /etc/modprobe.d/nouveau-kms.conf
+    fi
+    update-initramfs -u
 
-	# create startup script
-	cat <<EOF > $startup_script
+    # create startup script
+    cat <<EOF > $startup_script
 #!/bin/bash
 
 cd /root
@@ -132,10 +135,10 @@ rm $startup_script
 rm "NVIDIA-Linux-x86_64-$latest_driver_version.run"
 EOF
 
-	chmod +x $startup_script
+    chmod +x $startup_script
 
-	# create systemd service for automatic startup
-	cat <<EOF > $service_file
+    # create systemd service for automatic startup
+    cat <<EOF > $service_file
 [Unit]
 Description=Nvidia Driver Install
 
@@ -148,24 +151,38 @@ RemainAfterExit=yes
 WantedBy=multi-user.target
 EOF
 
-	systemctl enable nvidia_driver_install.service
+    systemctl enable nvidia_driver_install.service
 
-	printf '\n[+] Preparation finished.\n\n'
+    printf '\n[+] Preparation finished.\n\n'
 
-	read -p "[?] Reboot? (Y/N)" -r
-	
-	printf '\n[+] After reboot, feel free to check if driver is working:\n\n     $ lsmod | grep nvidia$\n'
-	printf "\n[+] Installation log is at $driver_log_file\n\n"
+    read -p "[?] Reboot? (Y/N)" -r
+    
+    printf '\n[+] After reboot, feel free to check if driver is working:\n\n     $ lsmod | grep nvidia$\n'
+    printf "\n[+] Installation log is at $driver_log_file\n\n"
 
-	sleep 4
+    sleep 4
 
-	if [[ $REPLY =~ ^[Yy]$ ]]
-	then
-		reboot
-	else
-		printf '[!] Please reboot "whenever"\n\n'
-		exit 2
-	fi
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        reboot
+    else
+        printf '[!] Please reboot "whenever"\n\n'
+        exit 2
+    fi
+
+}
+
+
+nvidia_runtime_install()
+{
+
+    printf '\n[+] Installing Nvidia Docker runtime\n'
+
+    curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | apt-key add -
+    distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+    curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+    apt-get update
+    apt-get install nvidia-container-runtime
 
 }
 
@@ -173,25 +190,25 @@ EOF
 main()
 {
 
-	check_root
+    check_root
 
-	# make sure we're running debian
-	command -v apt-get >>"$logfile" || (printf '\n[!] This script needs the APT package manager\n\n'; exit 1)
+    # make sure we're running debian
+    command -v apt-get >>"$logfile" || (printf '\n[!] This script needs the APT package manager\n\n'; exit 1)
 
-	get_latest_driver_version
+    get_latest_driver_version
 
-	if [ -z "$latest_driver_version" ]; then
-		printf '\n[!] Failed to detect driver version\n\n'
-		exit 1
-	fi
+    if [ -z "$latest_driver_version" ]; then
+        printf '\n[!] Failed to detect driver version\n\n'
+        exit 1
+    fi
 
-	package_install
+    package_install
 
-	hashcat_install
+    hashcat_install
 
-	driver_download
+    driver_download
 
-	prepare_driver_install
+    prepare_driver_install
 
 }
 
